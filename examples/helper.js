548caa7aa1801ab26c74b94d6105b16b2a1dc11d
Sten Ebenau <sten.ebenau@plauti.com>
2020-04-07 09:51:10 +0200

    js update

--- examples/helper.js
+++ examples/helper.js
@@ -1,14 +1,23 @@
 var jsforce = require('jsforce');
 var child_process = require('child_process');
-
+fs = require('fs');
 
 var helper = {};
 
 
 helper.getConnection = async function() {
-
-    var sfdxJson  = await child_process.execSync('sfdx force:org:display --json');
-    var jsonString = sfdxJson.toString('utf8');
+    
+    if (await fs.existsSync('./loginData.json')) {
+        var stat = await fs.statSync('./loginData.json');
+        var dif = Math.round((new Date().getTime() - stat.birthtime.getTime()) / 60000);
+        if (dif > 60) {
+            await child_process.execSync('sfdx force:org:display --json > ./loginData.json');
+        }
+    } else {
+        await child_process.execSync('sfdx force:org:display --json > ./loginData.json');
+    }
+    
+    var jsonString = await fs.readFileSync('./loginData.json');
     var json = JSON.parse(jsonString);
     
     var sfcon = new jsforce.Connection({
