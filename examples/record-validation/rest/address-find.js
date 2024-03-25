5228d4ef8067bc0f79ecb0ba0b8e7ef27b5b783e
Sten Ebenau <sten.ebenau@plauti.com>
2024-03-25 21:53:06 +0100

    Deleted the outdated retrieve endpoint

--- examples/record-validation/rest/address-find.js
+++ examples/record-validation/rest/address-find.js
@@ -1,16 +1,13 @@
-var helper = require('../../helper');
+var helper = require("../../helper");
 
 var process = async function () {
-
-    var con = await helper.getConnection();
-    var result = await con.apex.post('/recordval/v1/address/find', {
-        "address": 'Stationsplein 15 Amsterdam', 
-        "note": "demo", 
-        "country": "", //optional
-        "container": "" // optional: to retrieve sub-addresses in a street container suggestion.
-    });
-    console.dir(result, { depth: null });
-
-}
+  var con = await helper.getConnection();
+  var result = await con.apex.post("/recordval/v1/address/find", {
+    address: "432 Queslett Road",
+    note: "demo",
+    country: "GB",
+    container: "" // optional: to retrieve sub-addresses in a street container suggestion.
+  });
+  console.dir(result, { depth: null });
+};
 process();
-
--- examples/record-validation/rest/address-retrieve.js
+++ examples/record-validation/rest/address-retrieve.js
@@ -1,14 +1,11 @@
-var helper = require('../../helper');
+var helper = require("../../helper");
 
 var process = async function () {
-
-    var con = await helper.getConnection();
-    var result = await con.apex.post('/recordval/v1/address/retrieve', {
-        "container": 'NL|AV|A|B8348', // the container from the address find api endpoint. 
-        "note": "demo", 
-    });
-    console.dir(result, { depth: null });
-
-}
+  var con = await helper.getConnection();
+  var result = await con.apex.post("/recordval/v1/address/retrieve", {
+    container: "GB|RM|B|728895", // the container from the address find api endpoint.
+    note: "demo"
+  });
+  console.dir(result, { depth: null });
+};
 process();
-
