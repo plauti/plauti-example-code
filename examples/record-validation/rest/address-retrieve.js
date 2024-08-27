d7e2d51c743c9f9083741a9791f2167de2c0a7f5
Sten Ebenau <sten.ebenau@plauti.com>
2024-08-27 16:27:01 +0200

    RV Address Geocode Update

--- examples/record-validation/rest/address-retrieve.js
+++ examples/record-validation/rest/address-retrieve.js
@@ -1,11 +1,14 @@
-var helper = require("../../helper");
+var helper = require('../../helper');
 
 var process = async function () {
-  var con = await helper.getConnection();
-  var result = await con.apex.post("/recordval/v1/address/retrieve", {
-    container: "GB|RM|B|728895", // the container from the address find api endpoint.
-    note: "demo"
-  });
-  console.dir(result, { depth: null });
+    var con = await helper.getConnection();
+    var result = await con.apex.post('/recordval/v1/address/retrieve', {
+        container: 'GB|RM|B|728895', // the container from the address find api endpoint.
+        note: 'demo',
+        addressOptions: {
+            geocode: false
+        }
+    });
+    console.dir(result, {depth: null});
 };
 process();
--- examples/record-validation/rest/address-validate.js
+++ examples/record-validation/rest/address-validate.js
@@ -1,16 +1,15 @@
 var helper = require('../../helper');
 
 var process = async function () {
-
     var con = await helper.getConnection();
     var result = await con.apex.post('/recordval/v1/address/validate', {
-                "street": "Velperweg 8",
-                "country": "NL",
-                "note": "demo"
-              
+        street: 'Velperweg 8 arnhem',
+        country: 'NL',
+        note: 'demo',
+        addressOptions: {
+            geocode: false
+        }
     });
-    console.dir(result, { depth: null });
-
-}
+    console.dir(result, {depth: null});
+};
 process();
-
